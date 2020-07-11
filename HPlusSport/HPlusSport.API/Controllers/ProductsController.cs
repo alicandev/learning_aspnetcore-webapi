6490cb02c837c7a42c5058f2cce608e606ab23cf
Alian Demirtas <demirtasdev@hotmail.com>
2020-07-11 18:44:11 +0100

    HTTP Get

--- HPlusSport/HPlusSport.API/Controllers/ProductsController.cs
+++ HPlusSport/HPlusSport.API/Controllers/ProductsController.cs
@@ -2,14 +2,14 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HPlusSport.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class ProductsController : ControllerBase
     {
         [HttpGet]
-        public void GetProducts()
+        public string GetProducts()
         {
-            
+            return "Wow, it works!";
         }
     }
 }
