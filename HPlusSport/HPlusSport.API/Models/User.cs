6cb8c2064a67b37318ee6d660408e48b081fde0f
Alian Demirtas <demirtasdev@hotmail.com>
2020-07-11 18:52:42 +0100

    Data Model

---
+++ HPlusSport/HPlusSport.API/Models/User.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace HPlusSport.API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        
+        public virtual List<Order> Order { get; set; }
+    }
+}
